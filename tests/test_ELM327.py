956808269086f71f7d3f27bf14fb370457668a84
jptomoya <4786564+jptomoya@users.noreply.github.com>
2024-03-31 19:16:29 +0900

    Add OBD2Command test

--- tests/test_ELM327.py
+++ tests/test_ELM327.py
@@ -1,7 +1,7 @@
-import unittest
 import subprocess
 import signal
 import time
+import unittest
 from elm import Elm
 from elm327 import ELM327
 
@@ -27,14 +27,32 @@ class TestELM327(unittest.TestCase):
         self.assertEqual(elm.sendSingleCommand("ATE0"), 'OK')
         self.assertEqual(elm.sendSingleCommand("ATS0"), 'OK')
         self.assertEqual(elm.sendSingleCommand("ATH1"), 'OK')
-        self.assertRegex(elm.sendSingleCommand("ATI"), r"ELM327 v\d\.")
-        elm.sendSingleCommand("ATSP6")
+        self.assertRegex(elm.sendSingleCommand("ATI"), r"^ELM327 v\d\.\d")
         self.assertEqual(elm.sendSingleCommand("ATDP"), 'ISO 15765-4 (CAN 11/500)')
         self.assertTrue(elm.sendSingleCommand("ATDPN").endswith('6'))
 
     def test_readVoltage(self):
         elm = ELM327(self.port)
-        self.assertTrue(0.0 < elm.readVoltage() < 20.0)
+        self.assertTrue(0.0 <= elm.readVoltage() <= 20.0)
+
+    def test_OBD2Command(self):
+        elm = ELM327(self.port)
+        self.assertEqual(elm.sendSingleCommand("ATD"), 'OK')
+        self.assertEqual(elm.sendSingleCommand("ATSP6"), 'OK')
+        self.assertEqual(elm.sendSingleCommand("ATCAF1"), 'OK')
+        self.assertEqual(elm.sendSingleCommand("ATE0"), 'OK')
+        self.assertEqual(elm.sendSingleCommand("ATL0"), 'OK')
+        self.assertEqual(elm.sendSingleCommand("ATS0"), 'OK')
+        self.assertEqual(elm.sendSingleCommand("ATH1"), 'OK')
+        self.assertEqual(elm.sendSingleCommand("ATD0"), 'OK')
+
+        elm.sendUDSCommand("0100")  # HACK: perform auto search for ELM327-emulator
+        resp = elm.sendUDSCommand("0100")
+        self.assertIs(type(resp), list)
+        for x in resp:
+            self.assertIs(type(x), bytes)
+            if x not in (b'CAN ERROR', b'NO DATA'):
+                self.assertRegex(x, rb'^7[0-9a-fA-F]{16}$')
 
 if __name__ == '__main__':
     unittest.main()
